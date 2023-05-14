d21b9afbe0d01f7ee6984d0730f54a29062200e8
hieudao19x <hieudao19x@gmail.com>
2023-05-14 19:42:54 +0700

    Update service to singleton

--- src/demo/Main.java
+++ src/demo/Main.java
@@ -1,12 +1,16 @@
 package demo;
 import java.util.ArrayList;
+
 import entity.Member;
+import service.MemberService;
 import service.MemberServiceImpl;
 
 public class Main {
 
 	public static void main(String[] args) {
-		MemberServiceImpl service = new MemberServiceImpl();
+//		MemberServiceImpl service = new MemberServiceImpl();
+		MemberService service = MemberServiceImpl.getInstance();
+		
 		System.out.println(service.greet(2));
 		
 		System.out.println(service.getAll());
@@ -14,6 +18,8 @@ public class Main {
 		for(Member mem : list) {
 			System.out.println(mem.getId() + "," + mem.getName() + "," + mem.getEmail());
 		}
+		
+		System.out.println(service.sumOf(3, 5));
 	}
 
 }
