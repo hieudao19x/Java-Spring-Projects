f97bd526a400fd0fb1ff1996f403b78a31825118
hieudao19x <hieudao19x@gmail.com>
2023-06-12 00:11:13 +0700

    インクイリサービスを制作して、コントローラに使う

--- SpringBeginner/MyFirstApplication/src/main/java/com/example/demo/app/inquiry/InquiryController.java
+++ SpringBeginner/MyFirstApplication/src/main/java/com/example/demo/app/inquiry/InquiryController.java
@@ -1,5 +1,8 @@
 package com.example.demo.app.inquiry;
 
+import java.time.LocalDateTime;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -10,9 +13,20 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import com.example.demo.entity.Inquiry;
+import com.example.demo.service.InquiryService;
+
 @Controller
 @RequestMapping("/inquiry")
 public class InquiryController {
+	
+	private final InquiryService inquiryService;
+	
+	@Autowired
+	public InquiryController (InquiryService inquiryService) {
+		this.inquiryService = inquiryService;
+	}
+	
 	@GetMapping("/form")
 	public String form(InquiryForm inquiryForm, Model model, @ModelAttribute("complete") String complete) {
 		model.addAttribute("title", "Inquiry Form");
@@ -47,6 +61,14 @@ public class InquiryController {
 			model.addAttribute("title", "InquiryForm");
 			return "inquiry/form";
 		}
+		
+		Inquiry inquiry = new Inquiry();
+		inquiry.setName(inquiryForm.getName());
+		inquiry.setEmail(inquiryForm.getEmail());
+		inquiry.setContents(inquiryForm.getContents());
+		inquiry.setCreated(LocalDateTime.now());
+		
+		inquiryService.save(inquiry);
 		redirectAttributes.addFlashAttribute("complete","Registered!");
 		return "redirect:/inquiry/form";
 	}
